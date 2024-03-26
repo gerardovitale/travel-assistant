02c3e5efb70379533fec8e30f06783cf991f3d09
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2024-03-26 19:56:05 +0100

    improve spark session creation

--- travel_assistant/config.py
+++ travel_assistant/config.py
@@ -1,7 +1,7 @@
 import os
 
-import pytz
 from delta import configure_spark_with_delta_pip
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 
@@ -15,18 +15,20 @@ class Config:
         self.PARTITION_COLS = ["date", "hour"]
 
     def get_spark_session(self) -> SparkSession:
-        builder = (
-            SparkSession.builder.master("local[*]")
-            .appName(self.INSTANCE_NAME)
-            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-            .config("spark.driver.memory", "2g")
-            .config("spark.executor.memory", "1g")
-            .config("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
-            .config("spark.hadoop.fs.gs.project.id", self.PROJECT_ID)
-            .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
-            .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", self.GOOGLE_APPLICATION_CREDENTIALS)
+        spark_conf = SparkConf()
+        spark_conf.setAppName(self.INSTANCE_NAME)
+        spark_conf.setMaster("local[*]")
+        spark_conf.set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        spark_conf.set("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        spark_conf.set("spark.driver.memory", "2g")
+        spark_conf.set("spark.executor.memory", "1g")
+        spark_conf.set("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
+        spark_conf.set("spark.hadoop.fs.gs.project.id", self.PROJECT_ID)
+        spark_conf.set("spark.hadoop.google.cloud.auth.service.account.enable", "true")
+        spark_conf.set(
+            "spark.hadoop.google.cloud.auth.service.account.json.keyfile", self.GOOGLE_APPLICATION_CREDENTIALS
         )
+        builder = SparkSession.builder.config(conf=spark_conf)
         spark = configure_spark_with_delta_pip(builder).getOrCreate()
         spark.sparkContext.setLogLevel("ERROR")
         return spark
