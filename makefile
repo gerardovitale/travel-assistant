97d8a3179aa4dbf93f5a2a45e24ef85423c7c1b1
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2024-03-27 11:27:02 +0100

    add run locally adn update run docker

--- makefile
+++ makefile
@@ -8,17 +8,26 @@ export
 notebook:
 	poetry run jupyter-notebook
 
-test:
+unittest:
 	time poetry run pytest -vv --durations=0 .
 
-build:
-	docker build -t update_fuel_prices .
+build-docker:
+	docker build -f Dockerfile -t $(DOCKER_LOCAL_IMAGE_NAME) .
 
-run: build
+run-docker: build-docker
 	docker run --rm \
 	--name travel-assistant \
 	-v ./data/spain-fuel-price:/app/data/spain-fuel-price \
-	travass
+	$(DOCKER_LOCAL_IMAGE_NAME)
+
+clean-docker:
+	yes | docker container prune
+	yes | docker image prune
+	yes | docker volume prune
+
+run-locally:
+	cp $(GOOGLE_CLOUD_STORAGE_CONNECTOR) /Users/gerardovitaleerrico/Library/Caches/pypoetry/virtualenvs/travel-assistant-iA21hhyf-py3.9/lib/python3.9/site-packages/pyspark/jars
+	poetry run python3 travel-assistant/main.py
 
 publish: build
 	./scripts/docker-publish-update-fuel-prices.sh
--- scripts/docker-publish-update-fuel-prices.sh
+++ scripts/docker-publish-update-fuel-prices.sh
@@ -5,17 +5,12 @@ source scripts/common.sh
 COMMIT_ID=$(git rev-parse --short HEAD)
 
 logger "INFO" "Tagging images"
-docker tag update_fuel_prices gerardovitale/travel-assistant-update-fuel-prices:$COMMIT_ID
-docker tag update_fuel_prices gerardovitale/travel-assistant-update-fuel-prices:latest
+docker tag "$DOCKER_LOCAL_IMAGE_NAME" gerardovitale/travel-assistant-update-fuel-prices:"$COMMIT_ID"
+docker tag "$DOCKER_LOCAL_IMAGE_NAME" gerardovitale/travel-assistant-update-fuel-prices:latest
 
 logger "INFO" "Pushing images"
-docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD docker.io >/dev/null
-docker push gerardovitale/travel-assistant-update-fuel-prices:$COMMIT_ID
+docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD" docker.io >/dev/null
+docker push gerardovitale/travel-assistant-update-fuel-prices:"$COMMIT_ID"
 docker push gerardovitale/travel-assistant-update-fuel-prices:latest
 unset DOCKER_PASSWORD
 logger "INFO" "Push completed"
-
-logger "INFO" "Cleaning"
-yes | docker container prune
-yes | docker image prune
-yes | docker volume prune
