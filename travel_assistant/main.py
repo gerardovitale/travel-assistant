b50088cf48280bd7e063c6716cd1e2b5d7d68333
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2024-04-03 12:01:08 +0200

    remove unneeded import

--- travel_assistant/main.py
+++ travel_assistant/main.py
@@ -3,7 +3,6 @@ import logging
 from config import Config
 from dotenv import load_dotenv
 from fuel_price import update_spain_fuel_price_table
-from py4j.protocol import Py4JJavaError
 
 
 def main():
