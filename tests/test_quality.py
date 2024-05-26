d042c5a146e93ddbf60f3c34f8533410691f7a00
gerardovitale <gerardo.vitale.errico@gmail.com>
2024-05-26 15:22:05 +0200

    add event time and write_data_quality_metrics

--- tests/test_quality.py
+++ tests/test_quality.py
@@ -15,8 +15,14 @@ from travel_assistant.quality import data_quality_metrics
 
 class TestQuality(BaseTestCase):
 
+    def setUp(self):
+        logger_patch = patch("travel_assistant.quality.logger")
+        self.addCleanup(logger_patch.stop)
+        self.mock_logger = logger_patch.start()
+
     @patch("travel_assistant.quality.collect_metrics")
-    def test_data_quality_metrics_decorator(self, mock_collect_metrics: Mock):
+    @patch("travel_assistant.quality.write_data_quality_metrics")
+    def test_data_quality_metrics_decorator(self, mock_collect_metrics: Mock, mock_write_data_quality_metrics: Mock):
         test_schema = StructType([
             StructField("column1", StringType(), True),
             StructField("column2", StringType(), True),
@@ -37,6 +43,7 @@ class TestQuality(BaseTestCase):
         actual_df = test_func()
 
         mock_collect_metrics.assert_called_once()
+        mock_write_data_quality_metrics.assert_called_once()
         self.assert_spark_dataframes_equal(actual_df, expected_df)
 
     @patch("travel_assistant.quality.collect_metrics")
@@ -56,23 +63,23 @@ class TestQuality(BaseTestCase):
         mock_datetime.now.return_value = datetime(2024, 5, 25)
 
         test_schema = StructType([
+            StructField("dt", StringType(), True),
             StructField("column1", StringType(), True),
             StructField("column2", StringType(), True),
-            StructField("column3", StringType(), True),
         ])
         test_data = [
-            ("a", None, "a"),
-            ("b", None, "a"),
-            ("c", None, "a"),
-            ("d", None, None),
+            (datetime(2024, 1, 1).isoformat(), "a", None,),
+            (datetime(2024, 1, 1).isoformat(), "b", None,),
+            (datetime(2024, 1, 1).isoformat(), "c", None,),
+            (datetime(2024, 1, 1).isoformat(), None, None,),
         ]
         test_df = self.spark.createDataFrame(test_data, test_schema)
 
         expected_data = [
-            (datetime(2024, 5, 25), "Column", "column1", "completeness", 1.0),
-            (datetime(2024, 5, 25), "Column", "column2", "completeness", 0.0),
-            (datetime(2024, 5, 25), "Column", "column3", "completeness", 0.75),
-            (datetime(2024, 5, 25), "DataFrame", "size", "row_number", 4.0),
+            (datetime(2024, 5, 25).isoformat(), datetime(2024, 1, 1).isoformat(), "Column", "dt", "completeness", 1.0),
+            (datetime(2024, 5, 25).isoformat(), datetime(2024, 1, 1).isoformat(), "Column", "column1", "completeness", 0.75),
+            (datetime(2024, 5, 25).isoformat(), datetime(2024, 1, 1).isoformat(), "Column", "column2", "completeness", 0.0),
+            (datetime(2024, 5, 25).isoformat(), datetime(2024, 1, 1).isoformat(), "DataFrame", "size", "row_number", 4.0),
         ]
         expected_df = self.spark.createDataFrame(expected_data, QUALITY_SCHEMA)
 
@@ -92,10 +99,10 @@ class TestQuality(BaseTestCase):
         test_df = self.spark.createDataFrame(test_data, test_schema)
 
         expected_data = [
-            (datetime(2024, 5, 25), "Column", "column1", "completeness", 0.0),
-            (datetime(2024, 5, 25), "Column", "column2", "completeness", 0.0),
-            (datetime(2024, 5, 25), "Column", "column3", "completeness", 0.0),
-            (datetime(2024, 5, 25), "DataFrame", "size", "row_number", 0.0),
+            (datetime(2024, 5, 25).isoformat(), None, "Column", "column1", "completeness", 0.0),
+            (datetime(2024, 5, 25).isoformat(), None, "Column", "column2", "completeness", 0.0),
+            (datetime(2024, 5, 25).isoformat(), None, "Column", "column3", "completeness", 0.0),
+            (datetime(2024, 5, 25).isoformat(), None, "DataFrame", "size", "row_number", 0.0),
         ]
         expected_df = self.spark.createDataFrame(expected_data, QUALITY_SCHEMA)
 
--- travel_assistant/quality.py
+++ travel_assistant/quality.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 from functools import wraps
 
@@ -10,8 +11,13 @@ from pyspark.sql.types import StructField
 from pyspark.sql.types import StructType
 from pyspark.sql.types import TimestampType
 
+logger = logging.getLogger(__name__)
+
+# ISO_MILLIS_TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+BASE_QUALITY_PATH = "data/data-quality-metrics"
 QUALITY_SCHEMA = StructType([
-    StructField("datetime", TimestampType(), False),
+    StructField("processing_time", StringType(), False),
+    StructField("event_time", StringType(), True),
     StructField("entity", StringType(), False),
     StructField("instance", StringType(), False),
     StructField("metric_name", StringType(), False),
@@ -27,9 +33,14 @@ def data_quality_metrics(table_name: str):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            logger.info(f"Calling {func.__name__} with {args} and {kwargs}.")
             df = func(*args, **kwargs)
+            logger.info(f"Collecting data quality metrics for {table_name}.")
             metrics = collect_metrics(df)
+            logger.info(f"Data quality metrics for {table_name} collected.")
             metrics.show(truncate=False)
+            write_data_quality_metrics(metrics, table_name)
+            logger.info(f"Data quality metrics for {table_name} written.")
             return df
 
         return wrapper
@@ -42,12 +53,26 @@ def collect_metrics(df: DataFrame) -> DataFrame:
         raise NotDataFrameError(f"PySpark DataFrame was expected, instead got {type(df)}.")
 
     total_rows = float(df.count())
-    processing_dt = datetime.now()
-    metric_rows = [(processing_dt, "DataFrame", "size", "row_number", total_rows)]
+    processing_dt = datetime.now().isoformat()
+    event_time = df.select("dt").first().dt if "dt" in df.columns else None
+    metric_rows = [(processing_dt, event_time, "DataFrame", "size", "row_number", total_rows)]
 
     for column in df.columns:
         completeness = df.filter(col(column).isNotNull()).count() / (total_rows if total_rows > 0 else 1.0)
-        metric_rows.append((processing_dt, "Column", column, "completeness", completeness))
+        metric_rows.append((processing_dt, event_time, "Column", column, "completeness", completeness))
 
-    spark = SparkSession.builder.getOrCreate()
-    return spark.createDataFrame(metric_rows, QUALITY_SCHEMA)
+    return (
+        SparkSession.builder.getOrCreate()
+        .createDataFrame(metric_rows, QUALITY_SCHEMA)
+    )
+
+
+def write_data_quality_metrics(metrics: DataFrame, table_name: str) -> None:
+    table_path = f"{BASE_QUALITY_PATH}/{table_name}"
+    logger.info(f"Writing data quality metrics to {table_path}.")
+    (metrics.write
+     .format("delta")
+     .mode("append")
+     .option("mergeSchema", "true")
+     .save(table_path)
+     )
