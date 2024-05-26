017018eb286738243d52615a70b9774d4c53072e
gerardovitale <gerardo.vitale.errico@gmail.com>
2024-05-26 11:40:48 +0200

    test data_quality_metrics

--- tests/test_quality.py
+++ tests/test_quality.py
@@ -10,9 +10,47 @@ from tests.config_test import BaseTestCase
 from travel_assistant.quality import NotDataFrameError
 from travel_assistant.quality import QUALITY_SCHEMA
 from travel_assistant.quality import collect_metrics
+from travel_assistant.quality import data_quality_metrics
 
 
 class TestQuality(BaseTestCase):
+
+    @patch("travel_assistant.quality.collect_metrics")
+    def test_data_quality_metrics_decorator(self, mock_collect_metrics: Mock):
+        test_schema = StructType([
+            StructField("column1", StringType(), True),
+            StructField("column2", StringType(), True),
+        ])
+        test_data = [
+            ("a", None,),
+            ("b", None,),
+            ("c", None,),
+            ("d", None,),
+        ]
+
+        @data_quality_metrics("table_name")
+        def test_func():
+            return self.spark.createDataFrame(test_data, test_schema)
+
+        expected_df = self.spark.createDataFrame(test_data, test_schema)
+
+        actual_df = test_func()
+
+        mock_collect_metrics.assert_called_once()
+        self.assert_spark_dataframes_equal(actual_df, expected_df)
+
+    @patch("travel_assistant.quality.collect_metrics")
+    def test_data_quality_metrics_decorator_when_func_does_not_return_df(self, mock_collect_metrics: Mock):
+        mock_collect_metrics.side_effect = NotDataFrameError(
+            "PySpark DataFrame was expected, instead got <class 'NoneType'>."
+        )
+
+        @data_quality_metrics("table_name")
+        def test_func():
+            return None
+
+        self.assertRaises(NotDataFrameError, test_func)
+
     @patch("travel_assistant.quality.datetime")
     def test_collect_metrics(self, mock_datetime: Mock):
         mock_datetime.now.return_value = datetime(2024, 5, 25)
@@ -68,4 +106,3 @@ class TestQuality(BaseTestCase):
     def test_collect_metrics_when_invalid_output_is_passed(self):
         self.assertRaises(NotDataFrameError, collect_metrics, "invalid_output")
         self.assertRaises(NotDataFrameError, collect_metrics, None)
-
